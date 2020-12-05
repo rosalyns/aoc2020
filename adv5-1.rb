f45472360a8ac25d2bf59d0e9898ff13f1f72ab8
Rosalyn Sleurink <rosalyn.sleurink@nedap.com>
2020-12-05 09:38:21 +0100

    day 5

---
+++ adv5-1.rb
@@ -0,0 +1,24 @@
+example_input = File.readlines('adv5-1-input.txt', chomp: true)
+input = File.readlines('adv5-1-input2.txt', chomp: true)
+
+def algorithm(input)
+  max = 0
+  input.each do |line|
+    row = find_number(line[0..6])
+    column = find_number(line[7..9])
+    seat_id = seat_id(row, column)
+    if seat_id > max
+      max = seat_id
+    end
+    puts "row: #{row}, column: #{column}, seat id: #{seat_id}"
+  end
+  puts "highest: #{max}"
+end
+
+def find_number(directions)
+  directions.gsub('B', '1').gsub('F', '0').gsub('R', '1').gsub('L', '0').to_i(2)
+end
+
+def seat_id(row, column)
+  (row * 8) + column
+end
---
+++ adv5-2.rb
@@ -0,0 +1,22 @@
+example_input = File.readlines('adv5-1-input.txt', chomp: true)
+input = File.readlines('adv5-1-input2.txt', chomp: true)
+
+def algorithm(input)
+  seat_ids = []
+
+  input.each do |line|
+    row = find_number(line[0..6])
+    column = find_number(line[7..9])
+    seat_ids << seat_id(row, column)
+  end
+  puts "seat ids: #{seat_ids.sort}"
+  puts "missing: #{(53..896).to_a - seat_ids}"
+end
+
+def find_number(directions)
+  directions.gsub('B', '1').gsub('F', '0').gsub('R', '1').gsub('L', '0').to_i(2)
+end
+
+def seat_id(row, column)
+  (row * 8) + column
+end
