627cdc1a528152e9daa076208e7665ee8ed7b29f
Rosalyn Sleurink <rosalyn.sleurink@nedap.com>
2020-12-07 13:23:51 +0100

    day 7

---
+++ adv7-1.rb
@@ -0,0 +1,33 @@
+example_input = File.readlines('adv7-1-input.txt', chomp: true)
+input = File.readlines('adv7-input.txt', chomp: true)
+
+def algorithm(input, find_bag)
+  map = {}
+  input.each do |line|
+    captures = line.match(/^(.*) contain (.*)\.$/).captures
+    outside_bag = captures[0].match(/^(.*) bags$/).captures[0]
+    inside_bags = captures[1].split(', ').map { |bag| bag.match(/^(\d+|no) (.*) bags?$/).captures[1] }
+
+    inside_bags.each do |bag|
+      if map[bag]
+        map[bag] << outside_bag
+      else
+        map[bag] = [outside_bag]
+      end
+    end
+  end
+  puts map
+
+  puts "output: #{find_outer_bags(map, find_bag).count}"
+end
+
+def find_outer_bags(map, find_bag)
+  result = []
+  if map[find_bag]
+    result += map[find_bag]
+    result.each do |nested_bag|
+      result += find_outer_bags(map, nested_bag)
+    end
+  end
+  result.uniq
+end
---
+++ adv7-2.rb
@@ -0,0 +1,34 @@
+example_input = File.readlines('adv7-2-input.txt', chomp: true)
+input = File.readlines('adv7-input.txt', chomp: true)
+
+def algorithm(input, find_bag)
+  map = {}
+  input.each do |line|
+    captures = line.match(/^(.*) contain (.*)\.$/).captures
+    outside_bag = captures[0].match(/^(.*) bags$/).captures[0]
+    inside_bags = captures[1].split(', ')
+
+    inside_bags.each do |bag|
+      captures = bag.match(/^(\d+|no) (.*) bags?$/).captures
+      next if captures[0] == 'no'
+
+      if map[outside_bag]
+        map[outside_bag] << { amount: captures[0], color: captures[1] }
+      else
+        map[outside_bag] = [{ amount: captures[0], color: captures[1] }]
+      end
+    end
+  end
+  puts map
+
+  puts "output: #{count_bags(map, find_bag) - 1}"
+end
+
+def count_bags(map, find_bag)
+  sum = 1
+  map[find_bag]&.each do |inside_bag|
+    sum += inside_bag[:amount].to_i * count_bags(map, inside_bag[:color])
+  end
+
+  sum
+end
